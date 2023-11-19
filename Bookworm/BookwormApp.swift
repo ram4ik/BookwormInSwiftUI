b27b3681b4d8f55e58bd1799ce9290a99397e37d
ram4ik <ram4ik@hotmail.com>
2023-11-19 11:55:02 +0200

    Creating books with SwiftData

--- Bookworm/BookwormApp.swift
+++ Bookworm/BookwormApp.swift
@@ -5,6 +5,7 @@
 //  Created by Ramill Ibragimov on 19.11.2023.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct BookwormApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Book.self)
     }
 }
